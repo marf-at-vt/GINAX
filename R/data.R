dcab9c7f1cc1e34b39ee61caab534f326e6e9c0e
Xu <xshuangshuang@vtcri.local>
2024-11-19 10:32:27 -0500

    GINAX package

---
+++ R/data.R
@@ -0,0 +1,47 @@
+#' Example Dataset1: kinship
+#'
+#' This dataset contains kinship matrix.
+#'
+#' @format A matrix for kinship
+#' @source Generated for package example
+#' @name kinship
+#' @docType data
+"kinship"
+
+
+#' Example Dataset2: Y_poisson
+#'
+#' This dataset contains response variable (count data).
+#'
+#' @format A vector for poisson data
+#' @source Generated for package example
+#' @name Y_poisson
+#' @docType data
+"Y_poisson"
+
+
+#' Example Dataset3: Y_binary
+#'
+#' This dataset contains response variable (binary data).
+#'
+#' @format A vector for binary data
+#' @source Generated for package example
+#' @name Y_binary
+#' @docType data
+"Y_binary"
+
+
+#' Example Dataset4: SNPs
+#'
+#' This dataset contains all SNPs.
+#'
+#' @format A data frame with 328 rows and 9000 variables
+#' @source Generated for package example
+#' @name SNPs
+#' @docType data
+"SNPs"
+
+
+
+
+
